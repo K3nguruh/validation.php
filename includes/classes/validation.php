05f30fe08ac73caac1d8e206b2978cbbdc19201c
K3nguruh <k3nguruh@mail.de>
2024-03-21 15:46:01 +0100

    Komplette Überarbeitung  - Neu: setValue()  - Geändert: setCheck() zu setArray()

--- includes/classes/validation.php
+++ includes/classes/validation.php
@@ -6,47 +6,65 @@
  * Eine Klasse zur Validierung von Eingabedaten mit zusätzlichen Funktionen für Regeldefinition und Fehlerverwaltung.
  *
  * Autor:   K3nguruh <k3nguruh at mail dot de>
- * Version: 1.0.0
- * Datum:   2024-03-13 16:41
+ * Version: 1.1.0
+ * Datum:   2024-03-21 15:30
  * Lizenz:  MIT-Lizenz
  */
+
 class Validation extends Validate
 {
   private const DEFAULT_SEPARATOR = "||";
 
-  private $check;
+  private $value;
   private $alias;
+  private $cnt = 0;
   private $rules = [];
   private $errors = [];
 
   /**
-   * Legt die zu prüfenden Daten und das zugehörige Feld fest.
+   * Setzt den Wert für die Validierung.
    *
-   * Diese Methode akzeptiert ein Array ($array) und den Namen des zu prüfenden Felds ($name).
-   * Sie setzt das zu prüfende Datenfeld ($check) auf den entsprechenden Wert im Array.
-   * Das Alias ($alias) des Felds wird auf den angegebenen Feldnamen festgelegt.
-   * Die Methode initialisiert auch die Regeln ($rules) für die Validierung auf ein leeres Array.
+   * Diese Methode akzeptiert einen Wert und entfernt führende und abschließende Leerzeichen.
+   * Der Wert wird dann für die Validierung festgelegt, und ein Alias wird automatisch generiert,
+   * um eine eindeutige Identifikation während des Validierungsprozesses zu gewährleisten.
    *
-   * @param array $array Das Array, das die zu prüfenden Daten enthält.
-   * @param string $name Der Name des zu prüfenden Felds.
-   * @return $this Das Objekt selbst, um Methodenverkettungen zu ermöglichen.
+   * @param mixed $value Der Wert, der validiert werden soll.
+   * @return $this Das Validation-Objekt für Methodenverkettungen.
    */
-  public function setCheck($array, $name)
+  public function setValue($value)
   {
-    $this->check = trim($array[$name]);
-    $this->alias = $name;
-    $this->rules = [];
+    $this->value = trim($value);
+    $this->alias = $this->cnt++;
 
     return $this;
   }
 
   /**
-   * Legt das Alias für das zu prüfende Feld fest.
+   * Setzt den Wert aus einem Array basierend auf dem angegebenen Namen.
    *
-   * Diese Methode akzeptiert den Namen des zu prüfenden Felds ($name) und setzt das Alias ($alias) des Felds auf diesen Wert.
+   * Diese Methode extrahiert den Wert aus dem übergebenen Array unter Verwendung des angegebenen Namens.
+   * Der Wert wird dann für die Validierung festgelegt, und der angegebene Name wird als Alias verwendet.
    *
-   * @param string $name Der Name des zu prüfenden Felds.
-   * @return $this Das Objekt selbst, um Methodenverkettungen zu ermöglichen.
+   * @param array $array Das Array, aus dem der Wert entnommen wird.
+   * @param string $name Der Name des Werts im Array.
+   * @return $this Das Validation-Objekt für Methodenverkettungen.
+   */
+  public function setArray($array, $name)
+  {
+    $this->value = trim($array[$name]);
+    $this->alias = $name;
+
+    return $this;
+  }
+
+  /**
+   * Setzt einen Alias für den aktuellen Wert.
+   *
+   * Diese Methode setzt einen benutzerdefinierten Alias für den aktuellen Wert, um eine klarere Identifikation
+   * während des Validierungsprozesses zu ermöglichen.
+   *
+   * @param string $name Der Alias des aktuellen Werts.
+   * @return $this Das Validation-Objekt für Methodenverkettungen.
    */
   public function setAlias($name)
   {
@@ -56,46 +74,46 @@ class Validation extends Validate
   }
 
   /**
-   * Fügt eine Validierungsregel für das zu prüfende Feld hinzu.
+   * Fügt eine Validierungsregel hinzu.
    *
-   * Diese Methode akzeptiert eine Validierungsregel ($rule) und eine zugehörige Fehlermeldung ($message).
-   * Sie fügt das Regel-Array ["rule" => $rule, "message" => $message] zu den Regeln ($rules) für die Validierung hinzu.
+   * Diese Methode fügt eine Validierungsregel hinzu, die angibt, wie der Wert validiert werden soll,
+   * und die entsprechende Fehlermeldung im Falle eines Fehlers.
    *
-   * @param string $rule Die Validierungsregel, die hinzugefügt werden soll.
-   * @param string $message Die Fehlermeldung, die der Regel zugeordnet ist.
-   * @return $this Das Objekt selbst, um Methodenverkettungen zu ermöglichen.
+   * @param string $rule Die Validierungsregel.
+   * @param string $message Die Fehlermeldung im Falle eines Fehlers.
+   * @return $this Das Validation-Objekt für Methodenverkettungen.
    */
   public function setRule($rule, $message)
   {
-    $this->rules[] = ["rule" => $rule, "message" => $message];
+    $this->rules[$this->alias][] = ["rule" => $rule, "message" => $message];
 
     return $this;
   }
 
   /**
-   * Führt die Validierung des übergebenen Felds gemäß den festgelegten Validierungsregeln durch.
+   * Validiert den gesetzten Wert anhand der festgelegten Regeln.
    *
-   * Diese Methode durchläuft die festgelegten Validierungsregeln ($rules) für das zu prüfende Feld.
-   * Für jede Regel wird die entsprechende Validierungsmethode aufgerufen und überprüft, ob die Validierung erfolgreich ist.
-   * Wenn eine Regel verletzt wird, wird die entsprechende Fehlermeldung festgehalten und der Validierungsprozess abgebrochen.
-   * Am Ende wird das Objekt selbst zurückgegeben, um Methodenverkettungen zu ermöglichen.
+   * Diese Methode durchläuft alle festgelegten Regeln und überprüft den Wert gemäß jeder Regel.
+   * Im Falle eines Fehlers wird die entsprechende Fehlermeldung gespeichert.
    *
-   * @return $this Das Objekt selbst, um Methodenverkettungen zu ermöglichen.
+   * @return $this Das Validation-Objekt für Methodenverkettungen.
    * @throws InvalidArgumentException Wenn eine ungültige Validierungsregel gefunden wird.
    */
   public function validate()
   {
-    foreach ($this->rules as $rule) {
-      $args = explode(self::DEFAULT_SEPARATOR, $rule["rule"]);
-      $methodName = "validate" . ucfirst(array_shift($args));
+    foreach ($this->rules as $alias => $rules) {
+      foreach ($rules as $rule) {
+        $args = explode(self::DEFAULT_SEPARATOR, $rule["rule"]);
+        $methodName = "validate" . ucfirst(array_shift($args));
 
-      if (!method_exists($this, $methodName)) {
-        throw new InvalidArgumentException("Ungültige Validierungsregel: {$methodName}");
-      }
+        if (!method_exists($this, $methodName)) {
+          throw new InvalidArgumentException("Ungültige Validierungsregel: {$methodName}");
+        }
 
-      if (!$this->$methodName($this->check, ...$args)) {
-        $this->errors[$this->alias] = $rule["message"];
-        break;
+        if (!$this->$methodName($this->value, ...$args)) {
+          $this->errors[$alias] = $rule["message"];
+          break;
+        }
       }
     }
 
@@ -103,15 +121,20 @@ class Validation extends Validate
   }
 
   /**
-   * Gibt die gesammelten Fehlermeldungen zurück, die während des Validierungsprozesses aufgetreten sind.
+   * Gibt alle Fehlermeldungen zurück, die während der Validierung aufgetreten sind.
    *
-   * Diese Methode gibt ein Array von Fehlermeldungen zurück, das die Fehler für jedes validierte Feld enthält.
-   * Die Fehlermeldungen sind nach dem Feldnamen (Alias) indiziert.
+   * Diese Methode gibt alle gesammelten Fehlermeldungen zurück und setzt die Variablen zurück,
+   * für den Fall einer weiteren Validierung.
    *
-   * @return array Ein assoziatives Array von Fehlermeldungen, das die Fehler für jedes validierte Feld enthält.
+   * @return array Die Fehlermeldungen.
    */
   public function errors()
   {
-    return $this->errors;
+    $errors = $this->errors;
+    $this->cnt = 0;
+    $this->rules = [];
+    $this->errors = [];
+
+    return $errors;
   }
 }
